b7b5e7d4aa68741d6b686f5138f48a7594a5354e
MatthewWrenn <wrennmatt@comcast.net>
2025-04-17 23:25:18 -0700

    Update main.py Major updates to include more f strings and make it  more readable

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-# This will say hello and ask for your name and REJECT non-alphabetical characters
-print("Greeting and salutations!")
+# Asks the user for a name and rejects non-alphabetical characters
+print("Greetings and salutations!")
 while True:
-  myName = input("Might I have your name?")
-  if myName.replace(" ", "").isalpha():
-    break
-  print("Excuse me! I am programmed to accept only letters at this point.")
-print("Ahhh, what a wonderful name " + myName)
-print("Just the right number of leters")
-print(len(myName))
-#This is a is meant as a joke and will add 3 to the number you input also will REJECT non-numerical characters. Accepts floats though :D
-while True:
-  try:
-    chuckedwood = float(input("How much wood could a woodchuck chuck if a wood chuck could chuck wood?"))
-    break
-  except ValueError:
-    print("Excuse me! I am programmed to accept only numbers at this point.")
+    myName = input("Might I have your name? ")
+    if myName.replace(" ", "").isalpha():
+        break
+    print("Excuse me! I am programmed to accept only letters at this point.")
 
-print(f"Incorrect a Woodchuck can {round(chuckedwood + 3, 2)} chucks of wood!")
+print(f"Ahhh, what a wonderful name {myName}")
+print(f"It has {len(myName)} letters - just perfect!")
+
+# Woodchuck question - accepts only numbers and adds 3 to the response also rounds to 2 decimal places.
+while True:
+    try:
+        chuckedwood = float(input("How much wood could a woodchuck chuck if a woodchuck could chuck wood? "))
+        break
+    except ValueError:
+        print("Excuse me! I am programmed to accept only numbers at this point.")
+
+print(f"Incorrect! A woodchuck can {round(chuckedwood + 3, 2)} chucks of wood!")
