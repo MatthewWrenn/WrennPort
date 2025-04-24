d12eef8826277a732d65f09f8dc6ac029ffd3bb3
MatthewWrenn <wrennmatt@comcast.net>
2025-04-23 21:15:13 -0700

    Comment yeah?

--- Log_reader.py
+++ Log_reader.py
@@ -1,6 +1,9 @@
+#sets global pass/fail variables to 0
 fails = 0
 passes = 0
+#for loop for each of the 4 log files
 for i in range(1, 5):
+    #checks to see if it's the first log file
     if i == 1:
         with open('log_1.txt', 'r') as log:
          for line in log:
@@ -8,6 +11,7 @@ for i in range(1, 5):
                 fails += 1
             if 'PASS' in line:
                 passes += 1
+    #checks to see if it's the second log file
     elif i == 2:
         with open('log_2.txt', 'r') as log:
             for line in log:
@@ -15,6 +19,7 @@ for i in range(1, 5):
                     fails += 1
                 if 'PASS' in line:
                     passes += 1
+    #checks to see if it's the third log file
     elif i == 3:
         with open('log_3.txt', 'r') as log:
             for line in log:
@@ -22,6 +27,7 @@ for i in range(1, 5):
                     fails += 1
                 if 'PASS' in line:
                     passes += 1
+    #checks to see if it's the fourth log file
     else:
         with open('log_4.txt', 'r') as log:
             for line in log:
@@ -29,5 +35,5 @@ for i in range(1, 5):
                     fails += 1
                 if 'PASS' in line:
                     passes += 1
-
-print(f'Number of passes: {passes} and number of fails: {fails}')
+#prints the number of passes and fails
+print(f'Number of passes: {passes}\nNumber of fails: {fails}')
